46bafae82d32d49ab86488b402cab18bbadb607b
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2023-04-07 19:50:39 +0600

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -2,3 +2,5 @@
 <button onClick={() => handleClick(param)}>Click</button>
 
 // Higher order components [a function get a OriginalComponent as a parameter then return a NewComponent]
+
+// Render props pattern [component send as props and then render]
