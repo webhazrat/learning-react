e9462faa58d6f346f26dda0ae4367c91b6a54555
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2023-04-07 15:49:26 +0600

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,2 +1,4 @@
 // event function with param
 <button onClick={() => handleClick(param)}>Click</button>
+
+// Higher order components [a function get a OriginalComponent as a parameter then return a NewComponent]
