435a81f12da84a253e5cafb7849053c674df10a5
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2023-07-02 22:46:48 +0600

    Create useEffect.js

---
+++ useEffect.js
@@ -0,0 +1,5 @@
+// Some components need to synchronize with external systems. For example, you might want to control a non-React component based on the React state, set up a server connection, or send an analytics log when a component appears on the screen. Effects let you run some code after rendering so that you can synchronize your component with some system outside of React.
+
+useEffect(() => {
+  // do something
+}, [dependencies])
