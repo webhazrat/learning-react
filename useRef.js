dfbd7a9131cf680f467fa6969305a5b0ca388e95
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2023-07-02 22:25:43 +0600

    Update and rename useRef to useRef.js

---
+++ useRef.js
@@ -0,0 +1,5 @@
+// When you want a component to “remember” some information, but you don’t want that information to trigger new renders, you can use a ref.
+
+const ref = useRef(null);
+
+ref.current.[dom-functions]
